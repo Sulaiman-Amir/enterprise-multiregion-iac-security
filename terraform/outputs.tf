03fc4feb848552fac621da9d099687aa9c64bf4f
Sulaiman-Amir <SulleMaan007@gmail.com>
2025-07-07 01:21:23 +0500

    Add outputs to export VPC and bastion info

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,20 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.network.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public Subnet IDs"
+  value       = module.network.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "Private Subnet IDs"
+  value       = module.network.private_subnet_ids
+}
+
+output "bastion_public_ip" {
+  description = "Bastion Host Public IP"
+  value       = module.security.bastion_ip
+}
 
