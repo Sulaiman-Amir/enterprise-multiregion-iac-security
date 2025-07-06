62bb8839335cb0e47937e13f4366d12d49362db8
Sulaiman-Amir <SulleMaan007@gmail.com>
2025-07-07 01:20:04 +0500

    Add network module with secure VPC and subnets

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -1,1 +1,61 @@
+# terraform/modules/network/network.tf
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = element(var.azs, count.index)
+
+  tags = {
+    Name = "${var.env}-public-${count.index}"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "${var.env}-private-${count.index}"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+# NAT Gateway
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = element(aws_subnet.public[*].id, 0)
+
+  tags = {
+    Name = "${var.env}-nat"
+  }
+}
 
