753202b2a255e7f5c5fdc3e6135973702a60e8e3
Sulaiman-Amir <SulleMaan007@gmail.com>
2025-07-07 00:52:09 +0500

    Define core variables for AWS region, AZ, VPC, and subnet

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,24 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-west-2" # adjust to London
+}
+
+variable "aws_az" {
+  description = "AWS availability zone"
+  type        = string
+  default     = "eu-west-2a"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
 
