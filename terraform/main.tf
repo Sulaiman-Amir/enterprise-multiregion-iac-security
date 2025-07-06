d6f39659f3f47acea9e96dca06ba784ad63963c3
Sulaiman-Amir <SulleMaan007@gmail.com>
2025-07-07 00:55:56 +0500

    Add base VPC resource to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+# Add your subnets, security groups, etc, below
 
